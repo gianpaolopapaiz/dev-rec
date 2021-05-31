fba8d7d85b9bc10a562b1d2a2be6ddac969e704f
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2021-05-31 19:28:36 -0300

    customer and developer signup and added navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_developer!
+  before_action :authenticate_customer!
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_permitted_parameters_customer, if: :customers_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :skills, :years_experience, :github_url, :age, :phone])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :skills, :years_experience, :github_url, :age, :phone])
+  end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_developer!, only: :home
+  skip_before_action :authenticate_customer!, only: :home
   def home
   end
 end
