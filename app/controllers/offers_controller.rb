94211f2faf0e0ed0cedb0c59202aa87f4d8b3f99
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2021-06-01 15:18:39 -0300

    added customer show and offers show new create and destroy

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,4 +1,35 @@
 class OffersController < ApplicationController
+  def show
+    @offer = Offer.find(params[:id])
+  end
+  
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @offer.customer = current_customer
+    if @offer.save
+      redirect_to customer_path(current_customer)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @offer = Offer.find(params[:id])
+    @offer.destroy
+    redirect_to customer_path(@offer.customer)
+  end
+  
+
   def accept_proposal
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :description, :target_date)
   end
 end
