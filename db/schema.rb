0b2bed702c3ff0f3229985ce51dee99a9d45e16c
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2021-06-02 15:40:10 -0300

    added offer open logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_02_144556) do
+ActiveRecord::Schema.define(version: 2021_06_02_175144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2021_06_02_144556) do
     t.bigint "customer_id", null: false
     t.text "description"
     t.date "target_date"
-    t.boolean "open", default: true
+    t.boolean "offer_open", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
