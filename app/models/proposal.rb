ae1555a6dd9f37fdf5369aee72c73eafa7c406ea
Marcio Silva <mas16_06@hotmail.com>
2021-06-04 15:53:19 -0300

    proposal new changed, added Offer estimated date

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -2,7 +2,9 @@ class Proposal < ApplicationRecord
   belongs_to :offer
   belongs_to :developer
 
-  validates :price, :estimated_date, :details, presence: true
+  validates :price, :details, presence: true
+  validates :estimated_date, presence: true
+  #, less_than_or_equal_to: self.offer.target_date
   validates :status, inclusion: { in: %w[pending accepted rejected]}
   validates_numericality_of :price, :greater_than => 0.0
 end
